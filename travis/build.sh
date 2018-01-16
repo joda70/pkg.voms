f2762e753308a0753fa8701c9c0d5053ebe6064c
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2018-01-16 13:26:01 +0100

    travis: fixes for artifacts upload

--- travis/build.sh
+++ travis/build.sh
@@ -29,8 +29,15 @@ bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
 PING_LOOP_PID=$!
 
 pushd rpm
-export PKG_NEXUS_REPONAME="${PKG_NEXUS_HOST}/travis/${TRAVIS_REPO_SLUG}/${TRAVIS_JOB_ID}"
+export PKG_NEXUS_REPONAME="travis/${TRAVIS_REPO_SLUG}/${TRAVIS_JOB_ID}"
 bash build.sh >> ${BUILD_OUTPUT} 2>&1
 popd
 echo "pkg.voms build completed succesfully!"
 kill ${PING_LOOP_PID}
+
+BUILD_REPORT_URL=${PKG_NEXUS_HOST}/repository/${PKG_NEXUS_REPONAME}/travis-build.out
+curl --user "${PKG_NEXUS_USERNAME}:${PKG_NEXUS_PASSWORD}" --upload-file travis/travis-build.out \
+    ${PKG_NEXUS_HOST}/repository/${PKG_NEXUS_REPONAME}/travis-build.out
+
+echo "Build report available at:"
+echo ${BUILD_REPORT_URL}
